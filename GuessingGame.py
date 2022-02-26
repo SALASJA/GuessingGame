8d100ea86d1d7425f4cb1ef99707cf485d590f4b
Spoorthi Bedadam <spoorthibedadam@gmail.com>
2022-02-26 11:40:22 -0800

    added changes

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,9 +1,9 @@
 from random import randint
 def main():
 	game = GuessingGame()
-	guess = input("enter your guess: ")
+	guess = int(input("enter your guess: "))
 	while guess != game.get_mystery_number():
-		guess = input("enter your guess: ")
+		guess = int(input("enter your guess: "))
 	print("You Won")
 
 
@@ -14,9 +14,12 @@ class GuessingGame:
 		self.mystery_number = randint(0,10)
 	
 	def round(self, guess):
-		pass
+		if guess < self.mystery_number:
+			print("Too small")
+		if guess > self.mystery_number:
+			print("Too big")
 	
 	def get_mystery_number(self):
 		return self.mystery_number
-		
+
 main()
